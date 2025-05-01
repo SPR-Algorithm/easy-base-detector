8c4878aad72b436e922aea17ffd1777877f4cb3c
MyouKin <2517630545@qq.com>
2025-05-01 18:14:09 +0800

    添加 USB 设备规则文件和启动脚本，支持自动化启动

---
+++ Utils/start/autoaim_begin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/spr/easy-base-detector/
+
+/usr/bin/python3 /home/spr/easy-base-detector/base_detector.py
+
+cd /home/spr/easy-base-detector/Utils/start/
+
+./autoaim_begin1.sh
---
+++ Utils/start/autoaim_begin1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/spr/easy-base-detector/Utils/start/
+
+./autoaim_begin.sh
